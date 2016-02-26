740e9bca658904a64a594edbea3ac3b90ce89433
pavelpetrov <Pavel124578@mail.ru>
2016-02-26 16:32:27 +0300

    new Metod

--- src/ru/Pasha/Calculator.java
+++ src/ru/Pasha/Calculator.java
@@ -13,33 +13,32 @@ import java.lang.ref.SoftReference;
  */
 
 public class Calculator {
-    public static void main(String[] args) throws IOException {
+    static double summa = 0;
+    public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-
         while (true) {
 
-            System.out.println("Давайте вычеслим два положительных числа");
+            System.out.println("Давайте вычеслим два положительных числа и сосчитаем сумму вычислений");
             String inscription = reader.readLine();// вводим с клавиатуры строчку
-
             inscription = inscription.trim();
-
-            if (inscription.isEmpty())
+            if (inscription.isEmpty()) {
                 System.out.println("Вы ничего не ввели");
-            if(inscription.contains(" "))
-                metrodWithSpace(inscription);
-            else if(!inscription.contains(" "))
+            }
+            else if(inscription.contains("-")||inscription.contains("+")||inscription.contains("/")||inscription.contains("*")){
                 methodWithoutSpace(inscription);
+            }
+            else
+                System.out.println("Вы ввели вырожение неправильно");
         }
     }
-    public static void metrodWithSpace(String inscription ) { // метод с пробелом
+    public static void methodWithoutSpace(String inscription ) throws Exception {//метод без пробела
         Operatino operation = new Operatino();
 
         if (inscription.contains("*")) {
             int stringLeght = inscription.length();
             int index = inscription.indexOf("*");
-            String firstNumber = inscription.substring(0, index - 1);
-            String secongNumber = inscription.substring(index + 2, stringLeght);
+            String firstNumber = inscription.substring(0, index).trim();
+            String secongNumber = inscription.substring(index + 1, stringLeght).trim();
             errow(firstNumber);
             double firstNUmberDouble = Double.parseDouble(firstNumber);
             errow(secongNumber);
@@ -48,96 +47,8 @@ public class Calculator {
         } else if (inscription.contains("+")) {
             int stringLeght = inscription.length();
             int index = inscription.indexOf("+");
-            String firstNumber = inscription.substring(0, index - 1);
-            String secongNumber = inscription.substring(index + 2, stringLeght);
-            double firstNUmberDouble = Double.parseDouble(firstNumber);
-            double secongNUmberDouble = Double.parseDouble(secongNumber);
-            operation = new Addition(firstNUmberDouble, secongNUmberDouble);
-        } else if (inscription.contains("/")) {
-            int stringLeght = inscription.length();
-            int index = inscription.indexOf("/");
-            String firstNumber = inscription.substring(0, index - 1);
-            String secongNumber = inscription.substring(index + 2, stringLeght);
-            errow(firstNumber);
-            double firstNUmberDouble = Double.parseDouble(firstNumber);
-            errow(secongNumber);
-            double secongNUmberDouble = Double.parseDouble(secongNumber);
-            if (secongNUmberDouble == 0) {
-                System.out.println("На ноль делить нельзя");
-            } else
-                operation = new Dividing(firstNUmberDouble, secongNUmberDouble);
-        } else if (inscription.contains("-")) {
-            char[] list = inscription.toCharArray();
-            int minuscount = 0;
-            for (int i = 0 ; i < list.length; i ++){
-                if (list[i]=='-'){
-                    minuscount++;
-                }
-            }
-            if(minuscount == 1){
-                int stringLeght = inscription.length();
-                int index = inscription.indexOf("-");
-                String firstNumber = inscription.substring(0, index-1);
-                String secongNumber = inscription.substring(index + 2, stringLeght);
-                errow(firstNumber);
-                double firstNUmberDouble = Double.parseDouble(firstNumber);
-                errow(secongNumber);
-                double secongNUmberDouble = Double.parseDouble(secongNumber);
-                operation = new Substraction(firstNUmberDouble, secongNUmberDouble);
-            }
-            if (minuscount == 3){
-                for (int i = 1 ; i < list.length-2; i ++){
-                    if (list[i] == '-'){
-                        int stringLeght = inscription.length();
-                        int index = i;
-                        String firstNumber = inscription.substring(0, index-1);
-                        String secongNumber = inscription.substring(index + 2, stringLeght);
-                        errow(firstNumber);
-                        double firstNUmberDouble = Double.parseDouble(firstNumber);
-                        errow(secongNumber);
-                        double secongNUmberDouble = Double.parseDouble(secongNumber);
-                        operation = new Substraction(firstNUmberDouble, secongNUmberDouble);
-                        break;
-                    }
-                }
-            }
-            if (minuscount == 2){
-                for (int i = 1 ; i < list.length-2; i ++){
-                    if (list[i] == '-') {
-                        int stringLeght = inscription.length();
-                        int index = i;
-                        String firstNumber = inscription.substring(0, index-1);
-                        String secongNumber = inscription.substring(index + 2, stringLeght);
-                        errow(firstNumber);
-                        double firstNUmberDouble = Double.parseDouble(firstNumber);
-                        errow(secongNumber);
-                        double secongNUmberDouble = Double.parseDouble(secongNumber);
-                        operation = new Substraction(firstNUmberDouble, secongNUmberDouble);
-                        break;
-                    }
-                }
-            }
-        }
-        System.out.println(operation.run());
-    }
-    public static void methodWithoutSpace(String inscription ) {//метод без пробела
-        Operatino operation = new Operatino();
-
-        if (inscription.contains("*")) {
-            int stringLeght = inscription.length();
-            int index = inscription.indexOf("*");
-            String firstNumber = inscription.substring(0, index);
-            String secongNumber = inscription.substring(index + 1, stringLeght);
-            errow(firstNumber);
-            double firstNUmberDouble = Double.parseDouble(firstNumber);
-            errow(secongNumber);
-            double secongNUmberDouble = Double.parseDouble(secongNumber);
-            operation = new Multiplication(firstNUmberDouble, secongNUmberDouble);
-        } else if (inscription.contains("+")) {
-            int stringLeght = inscription.length();
-            int index = inscription.indexOf("+");
-            String firstNumber = inscription.substring(0, index);
-            String secongNumber = inscription.substring(index + 1, stringLeght);
+            String firstNumber = inscription.substring(0, index).trim();
+            String secongNumber = inscription.substring(index + 1, stringLeght).trim();
             errow(firstNumber);
             double firstNUmberDouble = Double.parseDouble(firstNumber);
             errow(secongNumber);
@@ -147,10 +58,10 @@ public class Calculator {
         } else if (inscription.contains("/")) {
             int stringLeght = inscription.length();
             int index = inscription.indexOf("/");
-            String firstNumber = inscription.substring(0, index);
-            String secongNumber = inscription.substring(index + 1, stringLeght);
+            String firstNumber = inscription.substring(0, index).trim();
             errow(firstNumber);
             double firstNUmberDouble = Double.parseDouble(firstNumber);
+            String secongNumber = inscription.substring(index + 1, stringLeght).trim();
             errow(secongNumber);
             double secongNUmberDouble = Double.parseDouble(secongNumber);
             if (secongNUmberDouble == 0) {
@@ -168,35 +79,40 @@ public class Calculator {
             if(minuscount == 1){
                  int stringLeght = inscription.length();
                  int index = inscription.indexOf("-");
-                 String firstNumber = inscription.substring(0, index);
-                 String secongNumber = inscription.substring(index + 1, stringLeght);
+                 String firstNumber = inscription.substring(0, index).trim();
+                 String secongNumber = inscription.substring(index + 1, stringLeght).trim();
                  errow(firstNumber);
                  double firstNUmberDouble = Double.parseDouble(firstNumber);
                  errow(secongNumber);
                  double secongNUmberDouble = Double.parseDouble(secongNumber);
                  operation = new Substraction(firstNUmberDouble, secongNUmberDouble);
             }
-            if (minuscount == 3){
+            else if (minuscount == 3){
                 for (int i = 1 ; i < list.length-2; i ++){
                     if (list[i] == '-'){
                         int stringLeght = inscription.length();
                         int index = i;
                         String firstNumber = inscription.substring(0, index);
                         String secongNumber = inscription.substring(index + 1, stringLeght);
-                        errow(firstNumber);
+                        if(!errow(firstNumber)){
+                            continue;
+                        }
                         double firstNUmberDouble = Double.parseDouble(firstNumber);
-                        errow(secongNumber);
+                        if (!errow(secongNumber)) {
+                            continue;
+                        }
                         double secongNUmberDouble = Double.parseDouble(secongNumber);
                         operation = new Substraction(firstNUmberDouble, secongNUmberDouble);
                         break;
                     }
                 }
             }
-            if (minuscount == 2){
+            else if (minuscount == 2){
                 for (int i = 1 ; i < list.length-1; i ++){
                     if (list[i] == '-') {
                         int stringLeght = inscription.length();
                         int index = i;
+
                         String firstNumber = inscription.substring(0, index);
                         String secongNumber = inscription.substring(index + 1, stringLeght);
                         if (!errow(firstNumber)) {
@@ -214,6 +130,8 @@ public class Calculator {
                 }
         }
         System.out.println(operation.run());
+        summa +=operation.run();
+        System.out.println(("SUmma = " + summa));
     }
     public static boolean errow(String firstNumber ){ //метод вывода ошибки
         boolean result = true;
@@ -224,8 +142,8 @@ public class Calculator {
                System.out.println("Пожалуйста вводите числа правильно");
                result = false;
            }
-
         return result;
+
     }
 }
 
